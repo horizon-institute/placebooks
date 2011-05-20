ead1f34f8ea00028aca13b1d60741a59c388eebe
Kevin Glover <ktg@cs.nott.ac.uk>
2011-05-20 15:07:25 +0100

    Change GPSTrace to store string instead of document, less toing and froing.

--- placebooks-webapp/src/placebooks/model/GPSTraceItem.java
+++ placebooks-webapp/src/placebooks/model/GPSTraceItem.java
@@ -1,27 +1,18 @@
 package placebooks.model;
 
-import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
 import java.net.URL;
 
 import javax.persistence.Entity;
+import javax.persistence.Lob;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
 import com.vividsolutions.jts.geom.Geometry;
 
@@ -29,10 +20,10 @@ import com.vividsolutions.jts.geom.Geometry;
 public class GPSTraceItem extends PlaceBookItem
 {
 	@JsonIgnore
-	private Document trace;
+	@Lob
+	private String trace;
 
-	public GPSTraceItem(final User owner, final Geometry geom, 
-						final URL sourceURL, final Document trace)
+	public GPSTraceItem(final User owner, final Geometry geom, final URL sourceURL, final String trace)
 	{
 		super(owner, geom, sourceURL);
 		this.trace = trace;
@@ -45,12 +36,24 @@ public class GPSTraceItem extends PlaceBookItem
 	@Override
 	public void appendConfiguration(final Document config, final Element root)
 	{
-		final Element item = getConfigurationHeader(config);
-		final Element traceElem = 
-			(Element)(trace.getElementsByTagName("gpx").item(0));
-		final Node traceNode = config.importNode(traceElem, true);
-		item.appendChild(traceNode);
-		root.appendChild(item);
+		try
+		{
+			final StringReader reader = new StringReader(trace);
+			final InputSource source = new InputSource(reader);
+			final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document document = builder.parse(source);
+			reader.close();
+
+			final Element item = getConfigurationHeader(config);
+			final Element traceElem = (Element) (document.getElementsByTagName("gpx").item(0));
+			final Node traceNode = config.importNode(traceElem, true);
+			item.appendChild(traceNode);
+			root.appendChild(item);
+		}
+		catch (Exception e)
+		{
+			log.info(e.getMessage(), e);
+		}
 	}
 
 	@Override
@@ -68,54 +71,11 @@ public class GPSTraceItem extends PlaceBookItem
 	// @Column(jdbcType = "CLOB")
 	public String getTrace()
 	{
-		try
-		{
-			final TransformerFactory tf = TransformerFactory.newInstance();
-			final Transformer t = tf.newTransformer();
-			final DOMSource source = new DOMSource(trace);
-
-			final StringWriter out = new StringWriter();
-			final StreamResult result = new StreamResult(out);
-			t.transform(source, result);
-			return out.getBuffer().toString();
-
-		}
-		catch (final TransformerConfigurationException e)
-		{
-			log.error(e.toString());
-		}
-		catch (final TransformerException e)
-		{
-			log.error(e.toString());
-		}
-
-		return null;
+		return trace;
 	}
 
 	public void setTrace(final String trace)
 	{
-		if (trace == null) { return; }
-		try
-		{
-			final StringReader reader = new StringReader(trace);
-			final InputSource source = new InputSource(reader);
-			final DocumentBuilder builder = 
-				DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			this.trace = builder.parse(source);
-			reader.close();
-		}
-		catch (final ParserConfigurationException e)
-		{
-			log.error(e.toString());
-		}
-		catch (final SAXException e)
-		{
-			log.error(e.toString());
-		}
-		catch (final IOException e)
-		{
-			log.error(e.toString());
-		}
+		this.trace = trace;
 	}
-
 }
