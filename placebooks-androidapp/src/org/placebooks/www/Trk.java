2c5b0427a03c4667a3518813b66dad261c4ca7bf
Mark Davies <markdavies@Mark-Daviess-MacBook-Pro.local>
2011-07-26 00:09:54 +0100

    Map YAH marker working accurately. Map trails are now working (example is hard-coded for now..I will change this properly to dynamic in the next day when the dev server is updated)

---
+++ placebooks-androidapp/src/org/placebooks/www/Trk.java
@@ -0,0 +1,29 @@
+package org.placebooks.www;
+
+import java.util.*;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+import org.simpleframework.xml.Root;
+
+//@Element(name="trk")
+public class Trk {
+	
+	@Element(required=false)
+	public String number;
+	
+	@ElementList
+    public ArrayList<Trkpt> trkseg;
+	
+	public ArrayList<Trkpt> getTrkseg() {
+         return trkseg;
+    }
+	
+	
+	public String getNumber(){
+		return number;
+	}
+	
+	
+}
+
