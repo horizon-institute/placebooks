e0d7c0df05d641e5bb27f6cb0314d5c1ce0eec15
Kevin Glover <ktg@cs.nott.ac.uk>
2011-08-16 15:40:21 +0100

    Added charset so server can determine character encoding.

--- placebooks-webapp/src/placebooks/client/PlaceBookService.java
+++ placebooks-webapp/src/placebooks/client/PlaceBookService.java
@@ -99,8 +99,9 @@ public class PlaceBookService
 		final RequestBuilder builder = new RequestBuilder(method, URL.encode(url));
 		if (data != null)
 		{
-			builder.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			builder.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
 			GWT.log("Request data: " + URL.decodePathSegment(data));
+			GWT.log("Request data: " + data);			
 		}
 		try
 		{
