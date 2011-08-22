1df18e16ffc657669b4e8aeb5820a8f398fcf760
Kevin Glover <ktg@cs.nott.ac.uk>
2011-08-22 11:53:55 +0100

    Fix z-index of set url dialog

--- placebooks-webapp/src/placebooks/client/ui/menuItems/SetSourceURLMenuItem.java
+++ placebooks-webapp/src/placebooks/client/ui/menuItems/SetSourceURLMenuItem.java
@@ -32,6 +32,7 @@ public class SetSourceURLMenuItem extends MenuItem
 		textBox.setValue(item.getItem().getSourceURL());
 
 		final PopupPanel dialogBox = new PopupPanel(false, true);
+		dialogBox.getElement().getStyle().setZIndex(2000);		
 
 		final Button closeButton = new Button("Set URL", new ClickHandler()
 		{
