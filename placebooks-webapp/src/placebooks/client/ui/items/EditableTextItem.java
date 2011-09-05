da990ddef621fbb6a56c7ff0c11cf8ab98da0ba5
Kevin Glover <ktg@cs.nott.ac.uk>
2011-09-05 10:26:20 +0100

    Fix middle column text size

--- placebooks-webapp/src/placebooks/client/ui/items/EditableTextItem.java
+++ placebooks-webapp/src/placebooks/client/ui/items/EditableTextItem.java
@@ -4,6 +4,7 @@ import placebooks.client.model.PlaceBookItem;
 import placebooks.client.resources.Resources;
 import placebooks.client.ui.widget.RichTextArea;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -73,9 +74,19 @@ public class EditableTextItem extends PlaceBookItemWidget
 		super.resize();
 		if (getParent() != null)
 		{
-			final double scale = getParent().getOffsetWidth() / 300d;
+			double panelWidth = 300;
+			if(getParent().getParent() != null && getParent().getParent().getParent() != null)
+			{
+				final String panelWidthString = getParent().getParent().getParent().getElement().getStyle().getWidth();				
+				if(panelWidthString != null && panelWidthString.endsWith("%"))
+				{
+					double percent = Double.parseDouble(panelWidthString.substring(0, panelWidthString.length() - 1));
+					panelWidth = (900d * percent) / 100d;
+				}
+			}
+			final double scale = getParent().getOffsetWidth() / panelWidth;
 			textPanel.getElement().setAttribute("style",
-												"width: 300px; -webkit-transform-origin: 0% 0%; -webkit-transform: scale("
+												"width: "+ panelWidth +"px; -webkit-transform-origin: 0% 0%; -webkit-transform: scale("
 														+ scale + ")");
 			return (getOffsetHeight() * scale) + "px";
 		}
