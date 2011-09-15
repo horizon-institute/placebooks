c3744145751b9d5e5bb21cb9d4df3c8e0b648db5
Mark Paxton <mcp@cs.nott.ac.uk>
2011-09-15 16:59:28 +0100

    fix import froe eventbus

--- placebooks-webapp/src/placebooks/client/PlaceBookEditor.java
+++ placebooks-webapp/src/placebooks/client/PlaceBookEditor.java
@@ -10,14 +10,14 @@ import com.google.gwt.activity.shared.ActivityManager;
 import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.place.shared.Place;
 import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.place.shared.PlaceHistoryHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
-import com.google.web.bindery.event.shared.EventBus;
-import com.google.web.bindery.event.shared.SimpleEventBus;
 
 public class PlaceBookEditor implements EntryPoint
 {
