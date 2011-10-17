95a769a1e1e834c659b23264c12d2d5778b0d809
Mark Paxton <mcp@cs.nott.ac.uk>
2011-10-17 17:18:52 +0100

    Added test case class missed from earlier commits (sorry for the mess!)

---
+++ placebooks-webapp/test/placebooks/test/PeoplesCollectionHelperTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package placebooks.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import placebooks.controller.PeoplesCollectionHelper;
+import placebooks.model.PeoplesCollectionLoginResponse;
+
+/**
+ * @author pszmp
+ *
+ */
+public class PeoplesCollectionHelperTest extends PlacebooksTestSuper {
+
+	/**
+	 * Test method for {@link placebooks.controller.PeoplesCollectionHelper#Login(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testLogin() {
+		PeoplesCollectionLoginResponse loginResponse =  PeoplesCollectionHelper.Login("fail-bad-user", "fail-bad-user");
+		assertFalse("Peoples Collection login indicated valid with bad username/password", loginResponse.GetIsValid());
+		assertNotNull("No failure reason given in login with bad user/password", loginResponse.GetReason());
+		
+		
+		loginResponse =  PeoplesCollectionHelper.Login("", "fail-no-user");
+		assertFalse("Peoples Collection login indicated valid with no username/password", loginResponse.GetIsValid());
+		assertNotNull("No failure reason given in login with no user/password", loginResponse.GetReason());
+
+		log.info("logging in as " + test_peoplescollection_username);
+		loginResponse =  PeoplesCollectionHelper.Login(test_peoplescollection_username, test_peoplescollection_password);
+		assertTrue("Login with correct username and password failed", loginResponse.GetIsValid());
+		assertNull("Response data should have been null in successful login", loginResponse.GetReason());
+
+	}
+
+}
