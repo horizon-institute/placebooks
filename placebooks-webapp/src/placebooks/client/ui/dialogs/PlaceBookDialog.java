3334c1b4c0d3da5ba2b66a99b23b7deb5ee20a3f
Kevin Glover <ktg@cs.nott.ac.uk>
2012-01-06 11:38:37 +0000

    Dialog style cleanup

--- placebooks-webapp/src/placebooks/client/ui/dialogs/PlaceBookDialog.java
+++ placebooks-webapp/src/placebooks/client/ui/dialogs/PlaceBookDialog.java
@@ -1,39 +1,13 @@
 package placebooks.client.ui.dialogs;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.ui.Widget;
 
 public abstract class PlaceBookDialog
 {
-	interface Bundle extends ClientBundle
-	{
-		@Source("PlaceBookDialog.css")
-		Style style();
-	}
-
-	interface Style extends CssResource
-	{
-		String dialog();
-
-		String dialogGlass();
-	}
-
-	private static final Bundle STYLES = GWT.create(Bundle.class);
-
 	private PlaceBookDialogFrame dialog = new PlaceBookDialogFrame();
 
 	public PlaceBookDialog()
-	{
-		STYLES.style().ensureInjected();
-
-		dialog.setGlassEnabled(true);
-		dialog.setAnimationEnabled(true);
-
-		dialog.setStyleName(STYLES.style().dialog());
-		dialog.setGlassStyleName(STYLES.style().dialogGlass());
-		dialog.setAutoHideEnabled(true);		
+	{	
 	}
 	
 	public void setWidget(final Widget widget)
