76b3821ea1790c870a1909915c7d0969b6e3f0d4
Kevin Glover <ktg@cs.nott.ac.uk>
2012-03-09 11:44:27 +0000

    Improved Map dialog

--- placebooks-webapp/src/placebooks/client/ui/dialogs/PlaceBookMapsDialog.java
+++ placebooks-webapp/src/placebooks/client/ui/dialogs/PlaceBookMapsDialog.java
@@ -3,39 +3,60 @@ package placebooks.client.ui.dialogs;
 import java.util.ArrayList;
 import java.util.List;
 
-import placebooks.client.model.LoginDetails;
 import placebooks.client.ui.elements.PlaceBookController;
 import placebooks.client.ui.images.markers.Markers;
 import placebooks.client.ui.items.MapItem;
 import placebooks.client.ui.items.PlaceBookItemWidget;
 import placebooks.client.ui.items.frames.PlaceBookItemFrame;
 
-import com.google.gwt.cell.client.ImageResourceCell;
+import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.ResourcePrototype;
+import com.google.gwt.safehtml.client.SafeHtmlTemplates;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.ImageResourceRenderer;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.NoSelectionModel;
 import com.google.gwt.view.client.SelectionChangeEvent;
-import com.google.gwt.view.client.SelectionModel;
 import com.google.gwt.view.client.SingleSelectionModel;
 
 public class PlaceBookMapsDialog extends PlaceBookDialog
 {
+	public interface MarkerTemplates extends SafeHtmlTemplates
+	{
+		@Template("<div style=\"text-align: center; padding-top: 2px;\">{0}</div>")
+		SafeHtml centeredDiv(SafeHtml image);
+	}
+
 	interface PlaceBookMapsDialogUiBinder extends UiBinder<Widget, PlaceBookMapsDialog>
 	{
 	}
+
+	private static class MarkerCell extends AbstractCell<ImageResource>
+	{
+		private static ImageResourceRenderer renderer = new ImageResourceRenderer();
+
+		@Override
+		public void render(final com.google.gwt.cell.client.Cell.Context context, final ImageResource value,
+				final SafeHtmlBuilder sb)
+		{
+			sb.append(MARKER_TEMPLATES.centeredDiv(renderer.render(value)));
+		}
+	}
+
+	private static final MarkerTemplates MARKER_TEMPLATES = GWT.create(MarkerTemplates.class);
 
 	private static PlaceBookMapsDialogUiBinder uiBinder = GWT.create(PlaceBookMapsDialogUiBinder.class);
 
@@ -44,23 +65,26 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 
 	@UiField
 	ListBox mapSelect;
-	
+
 	@UiField
 	Label mapLabel;
 
 	@UiField
 	CheckBox markerShow;
-	
+
+	@UiField
+	Panel mapRootPanel;
+
 	@UiField
 	Panel markerListPanel;
-	
+
 	private MapItem map;
 
 	private final PlaceBookItemWidget item;
 	private final List<PlaceBookItemFrame> mapItems;
 
 	private final PlaceBookController controller;
-	
+
 	private final CellList<ImageResource> markers;
 
 	public PlaceBookMapsDialog(final PlaceBookItemWidget item, final List<PlaceBookItemFrame> mapItems,
@@ -87,37 +111,37 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 			}
 		}
 
-		markers = new CellList<ImageResource>(new ImageResourceCell());
-		
-		List<ImageResource> markerList = new ArrayList<ImageResource>();
-		for(ResourcePrototype resource: Markers.IMAGES.getResources())
+		markers = new CellList<ImageResource>(new MarkerCell());
+
+		final List<ImageResource> markerList = new ArrayList<ImageResource>();
+		for (final ResourcePrototype resource : Markers.IMAGES.getResources())
 		{
-			if(resource instanceof ImageResource)
+			if (resource instanceof ImageResource)
 			{
 				markerList.add((ImageResource) resource);
 			}
 		}
-		
+
 		markers.setRowData(markerList);
-		
+
 		final SingleSelectionModel<ImageResource> selectionModel = new SingleSelectionModel<ImageResource>();
 		selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler()
 		{
 			@Override
-			public void onSelectionChange(SelectionChangeEvent event)
+			public void onSelectionChange(final SelectionChangeEvent event)
 			{
 				final ImageResource marker = selectionModel.getSelectedObject();
 				try
-				{	
-					int markerIndex = (int)marker.getName().charAt(6);
+				{
+					final int markerIndex = marker.getName().charAt(6);
 					item.getItem().setParameter("marker", markerIndex);
 				}
-				catch(Exception e)
+				catch (final Exception e)
 				{
 					item.getItem().removeParameter("marker");
-				}					
+				}
 				markerShow(null);
-				if(map != null)
+				if (map != null)
 				{
 					map.refreshMarkers();
 				}
@@ -126,32 +150,17 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 			}
 		});
 		markers.setSelectionModel(selectionModel);
-		
+
 		selectionModel.setSelected(item.getItem().getMarkerImage(), true);
-		
+
 		markerListPanel.add(markers);
-		
-		//markerSelect.setSelectedIndex(item.getItem().getParameter("marker", 0));
+
+		// markerSelect.setSelectedIndex(item.getItem().getParameter("marker", 0));
 		markerShow.setValue(item.getItem().getParameter("markerShow", 0) == 1);
-		
-		selectMap(mapPage);		
+
+		selectMap(mapPage);
 	}
 
-	@UiHandler("markerShow")
-	void markerShow(final ValueChangeEvent<Boolean> event)
-	{
-		if(markerShow.getValue())
-		{
-			item.getItem().setParameter("markerShow", 1);
-		}
-		else
-		{
-			item.getItem().removeParameter("markerShow");
-		}
-		controller.markChanged();
-		item.refresh();
-	}
-	
 	@UiHandler("mapSelect")
 	void mapSelected(final ChangeEvent event)
 	{
@@ -172,6 +181,21 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 		// controller.getContext().markChanged();
 	}
 
+	@UiHandler("markerShow")
+	void markerShow(final ValueChangeEvent<Boolean> event)
+	{
+		if (markerShow.getValue())
+		{
+			item.getItem().setParameter("markerShow", 1);
+		}
+		else
+		{
+			item.getItem().removeParameter("markerShow");
+		}
+		controller.markChanged();
+		item.refresh();
+	}
+
 	private void onInitialize()
 	{
 		mapSelect.clear();
@@ -181,8 +205,17 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 
 		for (final PlaceBookItemFrame item : mapItems)
 		{
-			mapSelect.addItem("On " + item.getItem().getMetadata("title", "Untitled") + " Map (page "
-					+ (item.getColumn().getPage().getIndex() + 1) + ")", Integer.toString(item.getColumn().getPage().getIndex()));
+			String title = item.getItem().getMetadata("title");
+			if (title == null || title.equals("Map"))
+			{
+				mapSelect.addItem(	"On Map on Page " + (item.getColumn().getPage().getIndex() + 1),
+									Integer.toString(item.getColumn().getPage().getIndex()));
+			}
+			else
+			{
+				mapSelect.addItem(	"On " + title + " Map (page " + (item.getColumn().getPage().getIndex() + 1) + ")",
+									Integer.toString(item.getColumn().getPage().getIndex()));
+			}
 		}
 	}
 
@@ -199,18 +232,16 @@ public class PlaceBookMapsDialog extends PlaceBookDialog
 
 		mapPanel.clear();
 
-		mapPanel.setVisible(page != -1);
-		mapLabel.setVisible(page != -1);
-		markers.setVisible(page != -1);
+		mapRootPanel.setVisible(page != -1);
 		markerShow.setVisible(page != -1);
 
 		if (item.getItem().getGeometry() == null)
 		{
-			mapLabel.setText("Click to Place " + item.getItem().getMetadata("title", "Untitled") + " on Map:");
+			mapLabel.setText("Click on the Map to Place " + item.getItem().getMetadata("title", "Untitled"));
 		}
 		else
 		{
-			mapLabel.setText("Click to Move " + item.getItem().getMetadata("title", "Untitled") + " on Map:");
+			mapLabel.setText("Click on the Map to Move " + item.getItem().getMetadata("title", "Untitled"));
 		}
 
 		if (page != -1)
