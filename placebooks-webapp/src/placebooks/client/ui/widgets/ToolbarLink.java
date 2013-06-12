b499145a5e2473e0873efa4f5a1eb3e8147f7f95
Kevin Glover <ktg@cs.nott.ac.uk>
2013-06-12 15:46:13 +0100

    More intelligent linking

--- placebooks-webapp/src/placebooks/client/ui/widgets/ToolbarLink.java
+++ placebooks-webapp/src/placebooks/client/ui/widgets/ToolbarLink.java
@@ -1,86 +1,33 @@
 package placebooks.client.ui.widgets;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HasText;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.impl.HyperlinkImpl;
 
 public class ToolbarLink extends Widget implements HasText
 {
+	private static final HyperlinkImpl impl = GWT.create(HyperlinkImpl.class);
+
 	private Image image = null;
 	private String text = null;
 	private String url = null;
 	private boolean enabled = true;
-	
+
 	public ToolbarLink()
 	{
 		setElement(DOM.createDiv());
 	}
 
-	public void setResource(ImageResource imageResource)
+	@Override
+	public String getText()
 	{
-		this.image = new Image(imageResource);
-		update();
-	}
-	
-	public void setEnabled(final boolean enabled)
-	{
-		this.enabled = enabled;
-		update();
-		if (enabled)
-		{
-			//setStylePrimaryName(style.enabled());
-		}
-		else
-		{
-			//setStylePrimaryName(style.disabled());
-		}
-	}
-	
-	public void setURL(String url)
-	{
-		this.url = url;
-		update();
-	}
-	
-	public void setText(String text)
-	{
-		this.text = text;
-		update();
-	}
-	
-	private String getInnerHTML()
-	{
-		if(image != null)
-		{
-			if(text != null)
-			{
-				return image.getElement().getString() + text;
-			}
-			else
-			{
-				return image.getElement().getString();
-			}
-		}
-		else if(text != null)
-		{
-			return text;
-		}
-		return "";
-	}
-	
-	private void update()
-	{
-		if(url != null && enabled)
-		{
-			getElement().setInnerHTML("<a href=\"" + url + "\">" + getInnerHTML() + "</a>");			
-		}
-		else
-		{
-			getElement().setInnerHTML(getInnerHTML());			
-		}
+		return text;
 	}
 
 	public boolean isEnabled()
@@ -89,8 +36,70 @@ public class ToolbarLink extends Widget implements HasText
 	}
 
 	@Override
-	public String getText()
+	public void onBrowserEvent(final Event event)
 	{
-		return text;
+		super.onBrowserEvent(event);
+		if (DOM.eventGetType(event) == Event.ONCLICK && impl.handleAsClick(event))
+		{
+			if(url.startsWith("#"))
+			{
+				History.newItem(url.substring(1));
+				DOM.eventPreventDefault(event);
+			}
+		}
+	}
+
+	public void setEnabled(final boolean enabled)
+	{
+		this.enabled = enabled;
+		update();
+	}
+
+	public void setResource(final ImageResource imageResource)
+	{
+		image = new Image(imageResource);
+		update();
+	}
+
+	@Override
+	public void setText(final String text)
+	{
+		this.text = text;
+		update();
+	}
+
+	public void setURL(final String url)
+	{
+		this.url = url;
+		update();
+	}
+
+	private String getInnerHTML()
+	{
+		if (image != null)
+		{
+			if (text != null)
+			{
+				return image.getElement().getString() + text;
+			}
+			else
+			{
+				return image.getElement().getString();
+			}
+		}
+		else if (text != null) { return text; }
+		return "";
+	}
+
+	private void update()
+	{
+		if (url != null && enabled)
+		{
+			getElement().setInnerHTML("<a href=\"" + url + "\">" + getInnerHTML() + "</a>");
+		}
+		else
+		{
+			getElement().setInnerHTML(getInnerHTML());
+		}
 	}
 }
